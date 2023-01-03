f12e661787250a4a88a906a280b7aa6cf1951c5a
Đorđe Božović <bozovicdjordje4@gmail.com>
2023-01-03 15:55:14 +0100

    slider configuration

--- gui/configuration_tab.py
+++ gui/configuration_tab.py
@@ -6,9 +6,10 @@ import pyqtgraph as pg
 import numpy as np
 
 from gui.configuration.graph_configuration import GraphConfiguration
-
+from gui.configuration.slider_configuration import SliderConfiguration
 
 class ConfigurationTab(GraphConfiguration):
     def __init__(self, window) -> None:
         super(ConfigurationTab, self).__init__(window)
+        self.slicer = SliderConfiguration(self.window)
 
