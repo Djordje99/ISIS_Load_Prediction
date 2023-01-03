d8f04733d7e8b12b199bdf36d9b7810885b8aea8
Đorđe Božović <bozovicdjordje4@gmail.com>
2023-01-03 17:46:13 +0100

    Update graph by slider change

--- gui/configuration_tab.py
+++ gui/configuration_tab.py
@@ -7,9 +7,11 @@ import numpy as np
 
 from gui.configuration.graph_configuration import GraphConfiguration
 from gui.configuration.slider_configuration import SliderConfiguration
+from gui.configuration.slider_graph_update import SliderGraphUpdate
 
 class ConfigurationTab(GraphConfiguration):
     def __init__(self, window) -> None:
         super(ConfigurationTab, self).__init__(window)
         self.slicer = SliderConfiguration(self.window)
+        self.graph_update = SliderGraphUpdate(self.window)
 
