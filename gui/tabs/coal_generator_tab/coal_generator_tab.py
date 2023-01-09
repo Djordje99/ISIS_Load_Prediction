af5cd091d928827d705a24cedc7aed06b088f573
Đorđe Božović <bozovicdjordje4@gmail.com>
2023-01-09 15:34:26 +0100

    Creating generator models initial

--- gui/tabs/coal_generator_tab/coal_generator_tab.py
+++ gui/tabs/coal_generator_tab/coal_generator_tab.py
@@ -8,3 +8,12 @@ class CoalGeneratorTab(CoalTabGraphConfiguration):
 
     def get_coal_generator_data(self):
         coal_consumption = self.get_coal_consumption_slider_value()
+        co2_emission = self.get_coal_co2_emission_slider_value()
+        max_power = self.window.max_thermal_coal_power_spin_box.value()
+        min_power = self.window.min_thermal_coal_power_spin_box.value()
+        coal_cost = self.window.coal_cost_spin_box.value()
+        power_plant_count = self.window.thermal_coal_count_spin_box.value()
+
+        return max_power, min_power, coal_cost, power_plant_count, coal_consumption, co2_emission
+
+
---
+++ load_optimization/generator_model_creator.py
@@ -0,0 +1,8 @@
+
+class GeneratorModelCreator():
+    def __init__(self) -> None:
+        pass
+
+
+    def create_thermal_coal_generator(self):
+        pass
