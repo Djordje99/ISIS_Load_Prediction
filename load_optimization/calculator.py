bccabc3ce3d09fff2d10f963d1d0740035d34196
Đorđe Božović <bozovicdjordje4@gmail.com>
2023-01-14 12:11:41 +0100

    Simplex with function bug

--- load_optimization/calculator.py
+++ load_optimization/calculator.py
@@ -11,7 +11,11 @@ from load_optimization.generator.solar import SolarGenerator
 
 from pulp import *
 
+import numpy as np
+
+
 AIR_DENSITY = 1.225
+
 
 class Calculator():
     def __init__(self, coal_generator:ThermalGenerator, gas_generator:ThermalGenerator, hydro_generator:HydroGenerator,
@@ -25,7 +29,13 @@ class Calculator():
         self.cost_weight = cost_weight
         self.co2_weight = co2_weight
 
-        self.simplex = Simplex(cost_weight, co2_weight)
+        COAL_POWER_RANGE = [i for i in np.linspace(self.coal_generator.min_production,
+                                            self.coal_generator.max_production, 6)]
+
+        GAS_POWER_RANGE = [i for i in np.linspace(self.gas_generator.min_production,
+                                            self.gas_generator.max_production, 6)]
+
+        self.simplex = Simplex(cost_weight, co2_weight, self.coal_generator.consumption_values, self.gas_generator.consumption_values, self.coal_generator.fuel_price, self.gas_generator.fuel_price, COAL_POWER_RANGE, GAS_POWER_RANGE)
 
         self.predicted_load_df = self.__load_predicted_load()
         self.weather_data = self.__load_weather_data(self.predicted_load_df['date'].min())
--- load_optimization/symplex.py
+++ load_optimization/symplex.py
@@ -1,12 +1,47 @@
-from scipy.optimize import linprog
 from pulp import *
+from scipy.interpolate import interp1d
+
+
+FUNCTION_RANGE = [0, 20, 40, 60, 80, 100]
+MAX_COAL_CONSUMPTION = 0.9 / 100
+MAX_GAS_CONSUMPTION = 0.03 / 100
+
 
 class Simplex():
-    def __init__(self, cost_weight, co2_weight) -> None:
+    def __init__(self, cost_weight, co2_weight, cost_consumption_coal, cost_consumption_gas, coal_price, gas_price, coal_power_range, gas_power_range) -> None:
         self.problem = LpProblem("Simple LP Problem", LpMinimize)
 
         self.cost_weight = cost_weight
         self.co2_weight = co2_weight
+
+        self.cost_consumption_coal = cost_consumption_coal.insert(0,0)
+        self.cost_consumption_gas = cost_consumption_gas.insert(0,0)
+
+        self.coal_price = coal_price
+        self.gas_price = gas_price
+
+        self.coal_power_range = coal_power_range
+        self.gas_power_range = gas_power_range
+
+
+    def get_coal_consumption_per_MW_function(self, x):
+        if x is None:
+            return 0
+
+        fun = interp1d(self.coal_power_range, self.cost_consumption_coal * MAX_COAL_CONSUMPTION , kind='linear')
+        value = fun(x)
+
+        return value
+
+
+    def get_gas_consumption_per_MW_function(self, x):
+        if x is None:
+            return 0
+
+        fun = interp1d(self.gas_power_range, self.cost_consumption_gas * MAX_GAS_CONSUMPTION, kind='linear')
+        value = fun(x)
+
+        return value
 
 
     @property
@@ -35,11 +70,11 @@ class Simplex():
 
         for key, variable in self.simplex_variable.items():
             if 'coal' in key:
-                objective_function += (self.cost_weight*230 + self.co2_weight*300)*variable
+                objective_function += variable*self.get_coal_consumption_per_MW_function(variable.varValue)*self.coal_price + 300*variable
             if 'gas' in key:
-                objective_function += (self.cost_weight*430 + self.co2_weight*240)*variable
+                objective_function += variable*self.get_gas_consumption_per_MW_function(variable.varValue)*self.gas_price + 240*variable
             if 'hydro' in key:
-                objective_function += (self.cost_weight*20 + self.co2_weight*30)*variable
+                objective_function += (20 + 30)*variable
 
         self.problem += objective_function
 
